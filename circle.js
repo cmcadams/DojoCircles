69dd387baec5e932d80831ac2a9cefb95da17047
Hafluto <45577374+Hafluto@users.noreply.github.com>
2018-12-04 22:50:37 +0000

    Revert "Update circle.js"
    This reverts commit 9e5ba70837c193f4752caac680167b68d3874d5b.

--- circle.js
+++ circle.js
@@ -17,32 +17,4 @@ this.randomiseDirection = function(){
 
 	this.direction = Math.floor(Math.random() * 4) + 1;
 };
-
-this.checkCollision = function(){
-	
-	if (circleCollision (this.circle, p2.circle) && this.type == "player1"){
-	console.log("Player Collision");		
-	//p1.randomisePos();
-	//p2.randomisePos();
-	}
-
-if (circleCollision(this.circle, heart.circle)){
-	console.log("Heart Collision");
-	//player.health+=1;
-	//heart.randomisePos();
-	}
-
-	if (circleCollision(this.circle, bomb.circle)){
-	console.log("Bomb Collision");
-	//player.health-=1;
-	//bomb.randomisePos()
-	}
-
-	if (circleCollision(this.circle, speedBoost.circle)){
-	console.log("Speed Boost Collision");
-	//player.speed *= speedMultiplier
-	//setTimeout(speedReset, 5000);
-	//speedBoost.randomisePos()
-	}
-};
 }

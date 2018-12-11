565a3bba083e80d4d12ad6d228364802c1c103ac
Hafluto <45577374+Hafluto@users.noreply.github.com>
2018-12-11 23:24:47 +0000

    Update superClass.js

--- superClass.js
+++ superClass.js
@@ -8,7 +8,7 @@ this.direction = direction;
 this.score = score;
 this.type = type;
 
-this.draw = function(){	
+this.draw = function(){
 	circle.draw();
 };
 this.move = function(speed){
@@ -28,41 +28,64 @@ switch (this.direction) {
 }
 };
 
-this.collidedWith = function(player, myCircle){
-	
-switch(myCircle.type) {
-    case "heart":
-       player.health+=1;
-	   console.log(player.health);
+this.collidedWithNP = function(np){
+
+switch(np.type) {
+    case HEART:
+       this.health+=1;
+	   console.log(this.health);
         break;
-    case "bomb":
-        player.health-=1;
-		console.log(player.health)
+    case BOMB:
+        this.health-=1;
+		console.log(this.health)
         break;
-    case "speedBoost":
-        //player.speed*=speedMultiplier;
+    case SPEEDBOOST:
+        //this.speed*=speedMultiplier;
 		console.log("hit speed boost");
-        break;
-	case "player2":
-	console.log("test");
-        p1.circle.randomisePos;
-        p2.circle.randomisePos;
         break;
 	}
 };
+this.collidedWithEdge = function(edge){
 
-}
+	switch(edge) {
+			case TOP:
+				 this.circle.p.y = this.circle.r;
+					break;
+			case BOTTOM:
+					this.circle.p.y = CANVASHEIGHT - this.circle.r;
+					break;
+			case RIGHT:
+					this.circle.p.x = CANVASWIDTH - this.circle.r;
+					break;
+		case LEFT:
+					this.circle.p.x = this.circle.r;
+					break;
+		}
+	}
+};
+
 function NonPlayer(circle, status, type){
 
 this.circle = circle;
 this.status = status;
 this.type = type;
 
-this.draw = function(){	
+this.draw = function(){
 	circle.draw();
 };
 
-this.randomisePos = function(){	
+this.randomisePos = function(){
 	circle.randomisePos();
+};
+}
+
+function Point(x, y){
+
+this.x = x;
+this.y = y;
+
+this.randomisePos = function(){
+	this.x = Math.floor(Math.random() * CANVASWIDTH) +1;
+	this.y = Math.floor(Math.random() * CANVASHEIGHT) +1;
 };
 }
